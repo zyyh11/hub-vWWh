8592adf096949d341310a91b3cc63a198d792a24
zyyh11 <zyyh14188@gmail.com>
2026-02-05 22:18:25 +0800

    Create config.py

---
+++ 张宇豪/week04/BERT情感分类/config.py
@@ -0,0 +1,6 @@
+MODEL_PATH = r'D:\huggingface\models\google-bert\bert-base-chinese'
+BERT_MODEL_PKL_PATH = './weights/bert.pt'
+
+CATEGORY_NAME = [
+    'sadness', 'joy', 'love', 'anger', 'fear', 'surprise'
+]
